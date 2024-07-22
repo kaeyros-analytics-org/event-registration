c9b3eda1eaa1738d55b71b3f2f45fd62ae47d9c9
Brice Tchakouna <brice.tchakouna@kaeyros-analytics.com>
2024-07-22 09:00:12 +0100

    update

--- src/core/controllers/event.controller.ts
+++ src/core/controllers/event.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { helpers } from "../../utils/helpers";
 import Event from "../database/schemas/event.schemas";
+import { eventService } from "./event.service";
 
 class EventController {
 
@@ -17,6 +18,8 @@ class EventController {
         const data = req.body
         const event = new Event(data)
         await event.save();
+        // const data
+        eventService.sendEmail(event.email, `Event Registration : ${event.first_name}`, 'test Hello content!')
         res.status(201).send(event);
     }
 }
