bace66be1928a2d7e54b640e6c0802792760e533
Brice Tchakouna <brice.tchakouna@kaeyros-analytics.com>
2024-10-14 14:16:10 +0100

    update for new form

--- src/core/controllers/events/event.service.ts
+++ src/core/controllers/events/event.service.ts
@@ -1,122 +1,128 @@
-import nodemailer from 'nodemailer';
-import ejs from 'ejs';
-import path from 'path';
-import { EventDoc, EventModel } from '../../models/events.models';
-import Event from '../../database/schemas/event.schemas';
-import XLSX from 'xlsx';
-import { NextFunction, Request, Response } from 'express';
+import nodemailer from "nodemailer";
+import ejs from "ejs";
+import path from "path";
+import { EventDoc, EventModel } from "../../models/events.models";
+import Event from "../../database/schemas/event.schemas";
+import XLSX from "xlsx";
+import { NextFunction, Request, Response } from "express";
 
+class EventService {
+  async sendEmail(
+    to: string,
+    subject: string,
+    data: EventModel,
+    nb_participant: number
+  ) {
+    const hostSTMP = process.env.EMAIL_HOST;
+    const STMPport = process.env.EMAIL_PORT || 465;
+    const STMPsecure = process.env.EMAIL_IS_SECURE || false;
+    console.log("process.env.EMAIL_IS_SECURE ==", STMPsecure);
+    console.log("process.env.EMAIL_IS_SECURE ==", STMPsecure);
 
-class EventService{
+    console.log("secure ==", Boolean(STMPsecure));
 
-    async sendEmail(to: string, subject: string, data: EventModel, nb_participant: number ){
-        const hostSTMP = process.env.EMAIL_HOST;
-        const STMPport = process.env.EMAIL_PORT || 465;
-        const STMPsecure = process.env.EMAIL_IS_SECURE || false;
-        console.log('process.env.EMAIL_IS_SECURE ==', STMPsecure);
-        console.log('process.env.EMAIL_IS_SECURE ==', STMPsecure);
-        
-        console.log('secure ==', Boolean(STMPsecure));
+    const dataEmail = {
+      company_name: data.company_name,
+      type_of_business: data.type_of_business,
+      promoter_name: data.promoter_name,
+      opening_time: data.opening_time,
+      phone_number_whatsapp: data.phone_number_whatsapp,
+      location: data.location,
+      latitude: data.latitude,
+      longitude: data.longitude,
+    };
 
-        const dataEmail = {
-            first_name: data.first_name,
-            last_name: data.last_name,
-            company_name: data.company_name,
-            sector_of_activity: data.sector_of_activity,
-            district: data.district,
-            poste: data.poste,
-            email: data?.email ?? '',
-            phone_number_whatsapp: data.phone_number_whatsapp,
-            creneau: data.creneau,
-            nb_participant: nb_participant,
-            is_promotion: data.is_promotion ? 'Oui': 'Non',
-            collaborate_name: data.collaborate_name
-        }
+    const html = await ejs.renderFile(
+      path.join(__dirname, `../../../../views/emails/event.ejs`),
+      dataEmail
+    );
 
-        const html= await ejs.renderFile(path.join(__dirname,`../../../../views/emails/event.ejs`), dataEmail);
-        
-        // Create transporter object using SMTP transport
-        let transporter = nodemailer.createTransport({
-            // service: 'Outlook365', // or any other email service
-            host: hostSTMP,
-            port: Number(STMPport),
-            secure: false,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-            tls: {
-                ciphers:'SSLv3'
-            }
-        });
+    // Create transporter object using SMTP transport
+    let transporter = nodemailer.createTransport({
+      // service: 'Outlook365', // or any other email service
+      host: hostSTMP,
+      port: Number(STMPport),
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        ciphers: "SSLv3",
+      },
+    });
 
-        // Email options
-        let mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: to,
-            subject: subject,
-            html: html,
+    // Email options
+    let mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: to,
+      subject: subject,
+      html: html,
+    };
+
+    // Send email
+    try {
+      let info = await transporter.sendMail(mailOptions);
+      console.log("info send ==", info);
+
+      return true;
+      // res.status(200).send({ message: 'Email sent', info: info });
+    } catch (error) {
+      console.log("error for send mail", error);
+      return false;
+      // res.status(500).send({ message: 'Error sending email', error: error });
+    }
+  }
+
+  async downloadDataExcel(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const event = await Event.find();
+
+      const data = event.map((item) => {
+        return {
+          company_name: item.company_name,
+          type_of_business: item.type_of_business,
+          promoter_name: item.promoter_name,
+          opening_time: item.opening_time,
+          phone_number_whatsapp: item.phone_number_whatsapp,
+          location: item.location,
+          latitude: item.latitude,
+          longitude: item.longitude,
         };
+      });
 
-        // Send email
-        try {
-            let info = await transporter.sendMail(mailOptions);
-            console.log('info send ==', info);
-            
-            return true
-            // res.status(200).send({ message: 'Email sent', info: info });
-        } catch (error) {
-            console.log('error for send mail', error);
-            return false;
-            // res.status(500).send({ message: 'Error sending email', error: error });
-        }
+      // Créer un nouveau classeur
+      const workbook = XLSX.utils.book_new();
+
+      // Convertir les données en feuille de calcul
+      const worksheet = XLSX.utils.json_to_sheet(data);
+
+      // Ajouter la feuille de calcul au classeur
+      XLSX.utils.book_append_sheet(workbook, worksheet, "event");
+
+      // Générer un fichier Excel en mémoire
+      const excelBuffer = XLSX.write(workbook, {
+        type: "buffer",
+        bookType: "xlsx",
+      });
+
+      // Définir les en-têtes HTTP pour télécharger le fichier
+      res.setHeader("Content-Disposition", 'attachment; filename="data.xlsx"');
+      res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      );
+
+      // Envoyer le fichier Excel au client
+      res.send(excelBuffer);
+    } catch (error) {
+      console.error(`Erreur lors du telechargement des datas ${error}`);
     }
-
-    async downloadDataExcel(req: Request, res: Response, next: NextFunction): Promise<any>
-    {
-        try {
-            const event = await Event.find();
-
-            const data = event.map(item => {
-                return {
-                    first_name: item.first_name,
-                    last_name: item.last_name,
-                    company_name: item.company_name,
-                    poste: item.poste,
-                    email: item.email,
-                    sector_of_activity: item.sector_of_activity,
-                    district: item.district,
-                    phone_number_whatsapp: item.phone_number_whatsapp,
-                    is_promotion: item.is_promotion ? 'true': 'false',
-                    creneau: item.creneau,
-                    latitude: item.latitude,
-                    longitude: item.longitude,
-                    collaborate_name: item.collaborate_name
-                }
-            })
-
-            // Créer un nouveau classeur
-            const workbook = XLSX.utils.book_new();
-
-            // Convertir les données en feuille de calcul
-            const worksheet = XLSX.utils.json_to_sheet(data);
-
-            // Ajouter la feuille de calcul au classeur
-            XLSX.utils.book_append_sheet(workbook, worksheet, 'event');
-
-            // Générer un fichier Excel en mémoire
-            const excelBuffer = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
-
-            // Définir les en-têtes HTTP pour télécharger le fichier
-            res.setHeader('Content-Disposition', 'attachment; filename="data.xlsx"');
-            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-
-            // Envoyer le fichier Excel au client
-            res.send(excelBuffer);
-        } catch (error) {
-            console.error(`Erreur lors du telechargement des datas ${error}`)
-        }
-    }
+  }
 }
 
 export const eventService = new EventService();
--- src/core/database/schemas/event.schemas.ts
+++ src/core/database/schemas/event.schemas.ts
@@ -1,69 +1,47 @@
-
 import mongoose, { Schema } from "mongoose";
 import { EventDoc } from "../../models/events.models";
 
 const eventSchema = new Schema<EventDoc>(
   {
-    first_name: {
-      type: String,
-      required: true,
-    },
-    last_name: {
-        type: String,
-        required: true,
-    },
     company_name: {
-        type: String,
-        required: true,
-    },
-    poste: {
       type: String,
       required: true,
     },
-    email: {
+    type_of_business: {
       type: String,
-      required: false
+      required: true,
     },
-    sector_of_activity: {
-      type: String,
-      required: true
-    },
-    district: {
-      type: String,
-      required: true
-    },
-    collaborate_name: {
+    promoter_name: {
       type: String,
       required: false,
-      default: ''
+    },
+    opening_time: {
+      type: String,
+      required: false,
+      default: "",
     },
     phone_number_whatsapp: {
-        type: String,
-        required: true
-    },
-    is_promotion: {
-      type: Boolean,
-      required: false,
-      default: false
-  },
-    creneau: {
       type: String,
-      required: true
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
     },
     latitude: {
       type: String,
-      required: false
+      required: false,
     },
     longitude: {
-        type: String,
-        required: false
-    }
+      type: String,
+      required: false,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const Event = mongoose.model<EventDoc>('events', eventSchema);
+const Event = mongoose.model<EventDoc>("eventsnew2", eventSchema);
 
 export default Event;
